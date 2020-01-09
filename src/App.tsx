83149b13893faa3c7a5470478163640c4c1290d4
Kapil Krishna <kapil.krishna.yo@gmail.com>
2020-01-09 16:03:22 +0000

    Added FetchData Method For API Call

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import {Navbar} from './components/general/navbar/Navbar';
 import {Person} from './components/general/Person';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Suspect from './components/Suspect';
 
 const App: React.FC = () => {
   return (
