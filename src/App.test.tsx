b46bf0e6915868a8f3b9d47338cbeb3d80bb3c58
Jake Horton <jake.horton@softwire.com>
2020-01-09 16:13:50 +0000

    Created Person Card

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,3 +6,8 @@ test('renders navbar', () => {
   const Navbar = document.createElement('Navbar');
   ReactDOM.render(<App />, Navbar);
 });
+
+test('renders person card', () => {
+  const Person = document.createElement('Person');
+  ReactDOM.render(<App />, Person);
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
-import {Navbar} from './components/general/navbar/navbar'
+import {Navbar} from './components/general/navbar/Navbar';
+import {Person} from './components/general/Person';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Navbar/>
+      <Person/>
     </Router>
   );
 }
-
 export default App;
