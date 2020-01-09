47e7f0eaacfda37345363b58f1ecce3838ffb54d
Jake Horton <jake.horton@softwire.com>
2020-01-09 15:08:11 +0000

    Deleted Comments on Nav Bar Test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
 import ReactDOM from 'react-dom';
-
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
 
 test('renders navbar', () => {
   const Navbar = document.createElement('Navbar');
