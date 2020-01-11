1b0f42fa6550fab60b0dcc0ca63abc9d6ff78495
Chris <chriskbrown@hotmail.co.uk>
2020-01-11 17:06:44 +0000

    Added Suspect Component & JSONFetcher common function

---
+++ src/components/suspect/Suspect.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import asyncJSONfetch from '../general/helpers/asyncJSONFetcher';
+
+interface SuspectInfo {
+    name: string;
+}
+
+export default function Suspect() {
+
+    const [suspectInfo, setSuspectInfo] = useState<SuspectInfo | null>(null);
+
+    const url: string = "https://techswitch-i-spy-api-staging.herokuapp.com/suspect"
+
+    useEffect(
+        () => { 
+        asyncJSONfetch(url)
+        .then(jsonResponse => setSuspectInfo(jsonResponse))
+    }, [url]);
+
+    if (!suspectInfo){
+        return (
+            <div>
+                Fetching data...
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            {suspectInfo.name}
+        </div>
+    )
+}
