91efade3f33e256c7891e370d83e17025c7138be
Chris <chriskbrown@hotmail.co.uk>
2020-01-23 11:00:29 +0000

    PublicHomePage Update

--- src/components/User-side/Homepage/PublicHomepage.tsx
+++ src/components/User-side/Homepage/PublicHomepage.tsx
@@ -6,26 +6,34 @@ import {asyncJSONFetch} from '../../general/helpers/asyncJSONFetcher';
 import { Suspect } from '../../general/helpers/SuspectInterfaces';
 import { AuthContext } from '../../AuthContext';
 
+interface JsonSuspectResponse{
+    items : Suspect[],
+    totalNumberOfItems: number,
+    previousPage: string | null,
+    nextPage: string | null
+}
+
 
 export function PublicHomepage(){
     
     const context = useContext(AuthContext);
-    const [suspectList, setSuspectList] = useState<Suspect[] | null>(null);
+    const [jsonList, setSuspectList] = useState<JsonSuspectResponse | null>(null);
     const [error, setError] = useState(false);
     let pageNumber :number = 1;
     const url: string = `${process.env.REACT_APP_API_URL}/suspects?page=${pageNumber}`
 
     useEffect(() => { 
         asyncJSONFetch(url)            
-            .then(jsonResponse => setSuspectList(jsonResponse))   
+            .then(jsonResponse => setSuspectList(jsonResponse))  
             .catch(() => setError(true))          
     }, [url]);    
 
+    console.log(jsonList)
     if (error) { 
         return <div>Oh No!!! There was an error</div> 
     }
 
-    if (!suspectList){
+    if (!jsonList){
         return (
             <div>Fetching data...</div>
         )
@@ -38,8 +46,8 @@ export function PublicHomepage(){
                 <h1>FBI Most Wanted</h1>
 
                 <ol className="suspectCardList"> 
-                    {suspectList.map(suspect => <div className ="suspectCardIndiv" data-testid = "SuspectCard">
-                        <SuspectCard name = {suspect.title} image = "suspect image"/></div>)}
+                    {jsonList.items.map(suspect => <div className ="suspectCardIndiv" data-testid = "SuspectCard">
+                        <SuspectCard name = {suspect.title} image = {suspect.imageUrl}/></div>)}
                 </ol>
 
                 <div className="buttons">
@@ -57,7 +65,7 @@ export function PublicHomepage(){
             <h1>FBI Most Wanted</h1>
 
             <ol className="suspectCardList"> 
-                {suspectList.map(suspect => <Link to={`/profile/${suspect.id}`} className ="suspectCardIndiv" data-testid = "SuspectCard"><SuspectCard name = {suspect.title} image = "suspect image"/></Link>)}
+                {jsonList.items.map(suspect => <Link to={`/profile/${suspect.id}`} className ="suspectCardIndiv" data-testid = "SuspectCard"><SuspectCard name = {suspect.title} image = {suspect.imageUrl}/></Link>)}
             </ol>
         </div>
     )
