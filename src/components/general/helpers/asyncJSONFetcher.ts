c3897752f1d20db0ae3f295fc6f591fbf6047d23
Jake Horton <jake.horton@softwire.com>
2020-01-23 17:16:05 +0000

    Fixed merge conflicts

--- src/components/general/helpers/asyncJSONFetcher.ts
+++ src/components/general/helpers/asyncJSONFetcher.ts
@@ -25,7 +25,7 @@ export async function asyncAdminJSONFetch(url: string,token :string) {
 export async function asyncJSONPostFetch(url: string,formData :FormData) {
     const response = await fetch(url,{
         method:'POST',
-        body: data
+        body: formData
     })
     console.log(response)
     return await response.json();
